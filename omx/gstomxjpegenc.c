1807b4b5a33acd9bbc62ba71f93cac4200408a6f
Carlos Gomez <carlos.gomez@ridgerun.com>
2018-04-09 12:15:06 -0600

    Fix issue setting quality property to omxjpegenc in NULL state, in this state calling set_format fails since the output port is required but created while going to READY state

--- omx/gstomxjpegenc.c
+++ omx/gstomxjpegenc.c
@@ -164,7 +164,8 @@ gst_omx_jpeg_enc_set_property (GObject * object, guint prop_id,
   switch (prop_id) {
     case PROP_QUALITY:
       self->quality = g_value_get_int (value);
-      gst_omx_jpeg_enc_set_format (GST_OMX_VIDEO_ENC (object), NULL, NULL);
+      if (GST_STATE_NULL != GST_STATE (object))
+        gst_omx_jpeg_enc_set_format (GST_OMX_VIDEO_ENC (object), NULL, NULL);
       break;
     default:
       G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
